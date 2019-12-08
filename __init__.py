2e7943f2db6286a8ec5b9abbfdc23646d7804e08
yung-ber <5ducksjesse@gmail.com>
2019-12-08 15:25:07 -0500

    program that will be imported from 'from py-sis import*'

---
+++ __init__.py
@@ -0,0 +1,23 @@
+# Python Script Install System
+from urllib.request import*
+from tkinter import*
+site=''
+class error(exception):
+	pass
+def install(self, name):
+	try:
+		file=open(name,'wb')
+		file.write(urlopen(site))
+		file.close()
+		self.wiz.destroy()
+		print('PySIS installed the software')
+	except:
+		raise error('failed to install file from'+site'!'\
+)
+class wizard:
+	def __init__(self, website, name='new'):
+		global site
+		self.wiz=Tk()
+		self.wiz.title('download wizard - powered by PySIS')
+		site=website
+		self.butt=Button(root, title='install software', command=install, args=self, name)
